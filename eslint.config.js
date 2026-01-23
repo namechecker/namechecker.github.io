dda132a08a5cad31e8f4ced297c9d864ef9ff723
Homun Mage <homun@posetmage.com>
2026-01-23 22:01:33 +0800

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -24,7 +24,15 @@ export default defineConfig(
 		rules: {
 			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
 			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-			'no-undef': 'off'
+			'no-undef': 'off',
+
+			'svelte/no-reactive-reassign': 'error',
+			'svelte/no-reactive-functions': 'error',
+			'svelte/require-store-reactive-access': 'error',
+			'svelte/no-immutable-reactive-statements': 'error',
+
+			// 🔥 kill the goto(resolve()) nonsense
+			'svelte/no-navigation-without-resolve': ['error', { ignoreGoto: true }]
 		}
 	},
 	{
